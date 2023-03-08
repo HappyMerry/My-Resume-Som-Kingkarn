3f865046365d5059a20aca47d0754a7e2544bb3d
Kingkarn <kingkarn.saipadit@gmail.com>
2023-03-09 00:39:18 +0700

    feat(resume): add resume content and style css

---
+++ src/ResumeContent/ResumeContent.jsx
@@ -0,0 +1,49 @@
+// Resume Content //
+
+function Content() {
+    return (
+      <main className='resume-content'>
+        <ul className='box-left'>
+          <li className='about-me'>
+            <h2>ABOUT ME</h2>
+            <p>
+                Hello! I like coding. I need to be Game Developer in the future. Nice to meet you.</p>
+          </li>
+          <li className='contact'>
+            <h2>CONTACT</h2>
+            <p>🍊 858 Samsennok, Huakwang, Bangkok, 10310</p>
+            <p>🍊 kingkarn.saipadit@gmail.com</p>
+            <p>🍊 +66-922561767</p>     
+          </li>
+          <li className='personal-skills'>
+            <h2>PERSONAL SKILLS</h2>
+            <p>🍊 HTML</p>
+            <p>🍊 CSS</p>
+            <p>🍊 JavaScript</p>
+            <p>🍊 English language</p>
+          </li>
+        </ul>
+        <ul className='box-right'>
+          <li className='education'>
+            <h2>EDUCATION</h2>
+            <h3>Srinakharinwirot university<br/>2012-2015</h3>
+            <p>Bachelor's degree of Public Administration.</p>
+          </li>
+          <li className='work-experience'>
+            <h2>WORK EXPERIENCE</h2>
+            <h3>HRIS Support<br/>2017 - 2018</h3>
+            <p>Contract employee in Human Resources Divition.</p>
+          </li>
+          <li className='hobbies'>
+            <h2>HOBBIES</h2>
+            <p>🍊 Travel</p>
+            <p>🍊 Dolls collector</p>
+            <p>🍊 Free dive</p>
+          </li>
+        </ul>
+  
+      </main>
+    );
+  }
+
+export default Content;
---
+++ src/ResumeHeader/ResumeHeader.jsx
@@ -0,0 +1,22 @@
+// Resume Header //
+
+function Header() {
+    return (
+      <header className='resume-header'>
+        <div className='resume-profile'>
+          <h1 className='resume-name'>Kingkarn<br/>Saipadit</h1>
+          <h3 className='resume-job'>Game Developer</h3>
+          <p className='resume-detail'>❝Resume about me. This resume have contact, education, personal skills, work experience and hobbies.❞</p>
+          <img className='resume-image' src='image\Luna.jpg' alt='profile image' />
+          <img className='resume-image-2' src='image\orang3.png' alt='image orange' />
+          <img className='resume-image-3' src='image\orang3.png' alt='image orange' />
+          <img className='resume-image-4' src='image\Black.png' alt='image black line' />
+          <img className='resume-image-5' src='image\Black.png' alt='image black line' />
+          <img className='resume-image-6' src='image\Black.png' alt='image black line' />
+        </div>
+      </header>
+    );
+  }
+
+  
+export default Header;

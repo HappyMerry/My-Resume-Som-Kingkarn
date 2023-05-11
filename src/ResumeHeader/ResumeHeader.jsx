9c158dc8c598b79a724e9574ae916045269c07bd
Kingkarn <kingkarn.saipadit@gmail.com>
2023-05-11 18:27:07 +0700

    feat(header): change job

--- src/ResumeHeader/ResumeHeader.jsx
+++ src/ResumeHeader/ResumeHeader.jsx
@@ -5,7 +5,7 @@ function Header() {
       <header className='resume-header'>
         <div className='resume-profile'>
           <h1 className='resume-name'>Kingkarn<br/>Saipadit</h1>
-          <h3 className='resume-job'>Game Developer</h3>
+          <h3 className='resume-job'>Developer Trainee</h3>
           <p className='resume-detail'>❝Resume about me. This resume have contact, education, personal skills, work experience and hobbies.❞</p>
           <img className='resume-image' src='image\Luna.jpg' alt='profile image' />
           <img className='resume-image-2' src='image\orang3.png' alt='image orange' />
